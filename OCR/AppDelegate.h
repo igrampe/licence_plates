1bb153b422371e742a32b1c86e098d86dde3cfd6
Sema Belokovsky <grampe@gmail.com>
2013-08-16 13:33:42 +0600

    init

--- OCR/AppDelegate.h
+++ OCR/AppDelegate.h
@@ -8,8 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
+@class VideoCaptureViewController;
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) VideoCaptureViewController *viewController;
 
 @end
